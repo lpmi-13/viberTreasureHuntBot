28223a8354f8d7abab8273c84e7f4b592811f862
lpmi-13 <leskis@gmail.com>
2017-02-02 12:29:33 +0000

    initial commit for basic echo test

---
+++ echo.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import Flask, request, Response
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
+import logging
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+app = Flask(__name__)
+
+bot_configuration = BotConfiguration(
+    name='TestyMcBot',
+    avatar='https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png',
+    auth_token=os.environ['VIBER_TOKEN']
+)
+
+viber = Api(bot_configuration)
+
+@app.route('/', methods=['POST'])
+def incoming():
+    logger.debug("received request. post data: {0}".format(request.get_data()))
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message + ', is what they all say...'
+        viber.send_message(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text='you are now subscribed to the madness!')
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn('client failed receiving message. failure: {0}'.format(viber_request))
+
+    return Response(status=200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5005) 
