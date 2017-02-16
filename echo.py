f6c8b70b4cd3966b2b9d8be35ac2858f24397c59
lpmi-13 <leskis@gmail.com>
2017-02-16 22:33:44 +0000

    update with keyboard and responses

--- echo.py
+++ echo.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.messages import PictureMessage
 import logging
 
 from viberbot.api.viber_requests import ViberConversationStartedRequest
@@ -30,27 +31,34 @@ bot_configuration = BotConfiguration(
 viber = Api(bot_configuration)
 
 keyboardResponse = {
-    "keyboard": {
-        "Type": "keyboard",
-        "DefaultHeight": true,
-        "BgColor": "#FFFFFF",
-        "Buttons": [{
-            "Columns": 6,
-            "Rows": 1,
-            "BgColor": "#2db9b9",
-            "BgMediaType": "gif",
-            "BgMedia": "http://www.url.by/test.gif",
-            "BgLoop": true,
-            "ActionType": "open-url",
-            "ActionBody": "www.tut.by",
-            "Image": "www.tut.by/img.jpg",
-            "Text": "Press Me!",
-            "TextVAlign": "middle",
-            "TextHAlign": "center",
-            "TextOpacity": 60,
-            "TextSize": "regular"
-        }]
-    }
+    "Type": "keyboard",
+    "DefaultHeight": True,
+    "BgColor": "#FFFFFF",
+    "Buttons": [{
+        "Columns": 3,
+        "Rows": 2,
+        "BgColor": "#008383",
+        "BgLoop": True,
+        "ActionType": "reply",
+        "ActionBody": "clue",
+        "Text": "get a clue",
+        "TextVAlign": "middle",
+        "TextHAlign": "center",
+        "TextOpacity": 60,
+        "TextSize": "regular"
+    }, {
+        "Columns": 3,
+        "Rows": 2,
+        "BgColor": "#7EDFDF",
+        "BgLoop" : True,
+        "ActionType": "reply",
+        "ActionBody": "sentence",
+        "Text": "correct a sentence",
+        "TextVAlign": "middle",
+        "TextHAlign": "center",
+        "TextOpacity": 60,
+        "TextSize": "regular"
+    }]
  }
 
 @app.route('/', methods=['POST'])
@@ -62,10 +70,17 @@ def incoming():
     viber_request = viber.parse_request(request.get_data())
 
     if isinstance(viber_request, ViberMessageRequest):
-        message = TextMessage(text='have a keyboard!', keyboardResponse) 
+        if viber_request.message.text == 'clue':
+            message = PictureMessage(media="https://github.com/lpmi-13/viberbot/blob/grammarbuffet/assets/locations/IMG_20170128_162050.jpg",text="find this place!")
+        elif viber_request.message.text == 'sentence':
+            message = TextMessage(text='its helpful to correct sentences.')
+        else:
+            message = TextMessage(text='have a keyboard!', keyboard=keyboardResponse) 
+
         viber.send_messages(viber_request.sender.id, [
             message
         ])
+
     elif isinstance(viber_request, ViberSubscribedRequest):
         viber.send_messages(viber_request.get_user.id, [
             TextMessage(text='you are now subscribed to the madness!')
